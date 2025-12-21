24a3e0e9033367f7d9359e2d713d3ac5b2a81ba3
雪银星月 <425899311@qq.com>
2025-12-21 22:28:53 +0800

    chore: 修改 Umami 环境变量

--- src/config.ts
+++ src/config.ts
@@ -649,7 +649,7 @@ export const widgetConfigs = {
 
 export const umamiConfig = {
 	enabled: true, // 是否显示Umami统计
-	apiKey: import.meta.env.UMAMI_API_KEY || "api_xxxxxxxx", // API密钥优先从环境变量读取，否则使用配置文件中的值
+	apiKey: import.meta.env.UMAMI_API_KEY || "api_Zvm7bu1e2PeAWOtFjIPjzlXUm8sutJaI", // API密钥优先从环境变量读取，否则使用配置文件中的值
 	baseUrl: "https://api.umami.is", // Umami Cloud API地址
 	scripts: `
 <script defer src="https://cloud.umami.is/script.js" data-website-id="a7e44097-94b4-4308-8cb8-66b718b9d93f"></script>
