fee5f7495e6953173e1eaad5c24901a49d021376
雪银星月 <425899311@qq.com>
2026-02-11 20:27:06 +0800

    fix: favicon 尺寸统一为 48x48

--- src/config.ts
+++ src/config.ts
@@ -190,7 +190,7 @@ export const siteConfig: SiteConfig = {
 		// {
 		//   src: '/favicon/icon.png',    // 图标文件路径
 		//   theme: 'light',              // 可选，指定主题 'light' | 'dark'
-		//   sizes: '32x32',              // 可选，图标大小
+		//   sizes: '48x48',              // 可选，图标大小
 		// }
 	],
 
--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -4,11 +4,11 @@ export const defaultFavicons: Favicon[] = [
 	{
 		src: "/favicon/favicon.ico",
 		theme: "light",
-		sizes: "64x64",
+		sizes: "48x48",
 	},
 	{
 		src: "/favicon/favicon.ico",
 		theme: "dark",
-		sizes: "64x64",
+		sizes: "48x48",
 	},
 ];
